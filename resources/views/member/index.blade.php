1ebf79ef4c6bbc261ab77fd0a4ca70071775d112
mtungij <jamesmtungi2013@gmail.com>
2026-01-21 10:52:09 +0300

    hello

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -6,7 +6,7 @@
         <!-- Start coding here -->
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-                <div class="w-full md:w-1/2">
+                <div class="w-full">
                     <form class="flex items-center" method="GET" action="{{ route('members.index') }}" id="searchForm">
                         <label for="simple-search" class="sr-only">Search</label>
                         <div class="relative w-full">
