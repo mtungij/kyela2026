9d0f213ab932ec12201dd2c568d6fff8ef87bb1d
mtungij <jamesmtungi2013@gmail.com>
2026-01-26 13:48:35 +0300

    hello

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,12 +1,11 @@
-
 <x-layouts.auth>
-    <div class="flex flex-col gap-6">
+    <div class="flex flex-col gap-6 max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md border border-gray-300">
 
-          <div class="flex justify-center mb-4 ">
+        <div class="flex justify-center mb-4">
             <img src="{{ asset('images/logo.jpeg') }}" alt="Logo" class="h-36 w-auto rounded-full">
         </div>
 
-        <x-auth-header :title="__('Ingia kwenye akaunti yako')" :description="__('Ingiza barua pepe na nenosiri lako hapa ili kuingia')" />
+    
 
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -40,26 +39,36 @@
 
                 @if (Route::has('password.request'))
                     <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
-                        {{ __('Forgot your password?') }}
+                        {{ __('') }}
                     </flux:link>
                 @endif
             </div>
 
-            
 
-          <div class="flex items-center justify-end">
-    <flux:button
-        variant="primary"
-        type="submit"
-        class="w-full bg-[#0B3D2E] hover:bg-[#093327] text-white transition-colors"
-        data-test="login-button"
+           <!-- Msaada Section -->
+<p class="text-center text-sm text-gray-600 mt-4">
+    Kwa changamoto ama msaada zaidi, wasiliana na injinia wa mfumo: 
+    <a 
+        href="https://wa.me/255747384847" 
+        target="_blank" 
+        class="font-semibold text-[#0B3D2E] hover:underline"
     >
-        {{ __('Log in') }}
-    </flux:button>
-</div>
+        0747384847
+    </a>
+</p>
 
+
+            <div class="flex items-center justify-end">
+                <flux:button
+                    variant="primary"
+                    type="submit"
+                    class="w-full bg-[#0B3D2E] hover:bg-[#093327] text-white transition-colors"
+                    data-test="login-button"
+                >
+                    {{ __('Log in') }}
+                </flux:button>
+            </div>
         </form>
 
-      
     </div>
 </x-layouts.auth>
